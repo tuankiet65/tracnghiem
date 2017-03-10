449108435f4ceec50822a38009d7406d6a240b33
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-03-10 13:26:48 +0700

    [admin] Enable authentication

--- tracnghiem/admin/__init__.py
+++ tracnghiem/admin/__init__.py
@@ -8,32 +8,21 @@ admin = Blueprint("admin", __name__,
                   url_prefix = "/admin")
 
 from .authentication import login_page
-
-admin.add_url_rule("/login", view_func = login_page, methods = ["GET", "POST"])
-
 from .routes import index
-
-admin.add_url_rule("/", view_func = index)
-
 from .announcement import AnnouncementList
-
-AnnouncementList.add_url_rule(admin)
-
 from .school import SchoolList
-
-SchoolList.add_url_rule(admin)
-
 from .contest import ContestList
-
-ContestList.add_url_rule(admin)
-
 from .questions import QuestionList
-
-QuestionList.add_url_rule(admin)
-
 from .question_set import QuestionSetList
 
+admin.add_url_rule("/login", view_func = login_page, methods = ["GET", "POST"])
+admin.add_url_rule("/", view_func = index)
+AnnouncementList.add_url_rule(admin)
+SchoolList.add_url_rule(admin)
+ContestList.add_url_rule(admin)
+QuestionList.add_url_rule(admin)
 QuestionSetList.add_url_rule(admin)
+
 
 @admin.before_request
 def load_user_info():
@@ -42,6 +31,4 @@ def load_user_info():
     except KeyError:
         g.user = None
     if g.user is None and request.path != url_for("admin.login_page"):
-        # TODO: Enable this in production
-        redirect(url_for("admin.login_page"))
-
+        return redirect(url_for("admin.login_page"))
