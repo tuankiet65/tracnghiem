38874d30a55180730b3f031084dc26a2d77b73f9
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-04-27 14:32:33 +0700

    [stats] Use hand-crafted SQL statement
    Should result in a 2x performance increase.

--- tracnghiem/static/js/index.js
+++ tracnghiem/static/js/index.js
@@ -1,16 +1,22 @@
 statistics = JSON.parse(statistics);
 
 function change_stats(val){
-    account_count = statistics[val].account_count;
-    exam_count = statistics[val].exam_count;
+    var account_count, exam_count;
+    if (val in statistics) {
+        account_count = statistics[val].account_count;
+        exam_count = statistics[val].exam_count;
+    } else {
+        account_count = 0;
+        exam_count = 0;
+    }
 
     $("#stats-account-count").text(account_count);
     $("#stats-exam-count").text(exam_count);
 }
 
 $('#stat-choose-school').change(function(){
-    val = $('#stat-choose-school').val();
+    var val = $('#stat-choose-school').val();
     change_stats(val);
-})
+});
 
 change_stats($('#stat-choose-school').val());
--- tracnghiem/stats.py
+++ tracnghiem/stats.py
@@ -1,13 +1,42 @@
-from .database import Account, Exam, School
+from .database import School, Account, Exam
 
 
 def generate_stats():
     result = {}
-    schools = School.select()
 
-    for school in schools:
-        result[school.id] = {}
-        result[school.id]['account_count'] = Account.select().join(School).where(Account.school == school).count()
-        result[school.id]['exam_count'] = Exam.select().join(Account).where(Account.school == school).count()
+    # The below SQL + Python code should be equivalent to this
+    # schools = School.select()
+    # for school in schools:
+    #     result[school.id] = {}
+    #     result[school.id]['account_count'] = Account.select().join(School).where(Account.school == school).count()
+    #     result[school.id]['exam_count'] = Exam.select().join(Account).where(Account.school == school).count()
+
+    query = School.raw("""SELECT
+        t1.school_id,
+        account_count,
+        exam_count
+    FROM
+        ((SELECT
+            account.school_id AS school_id, COUNT(*) AS account_count
+        FROM
+            tracnghiem.account
+        GROUP BY account.school_id) AS t1)
+            INNER JOIN
+        ((SELECT
+            school.id AS school_id, COUNT(*) AS exam_count
+        FROM
+            ((tracnghiem.exam
+        INNER JOIN tracnghiem.account ON exam.contestant_id = account.id)
+        INNER JOIN tracnghiem.school ON account.school_id = school.id)
+        GROUP BY school.id) AS t2) ON t1.school_id = t2.school_id
+    """).dicts().execute()
+
+    for row in query:
+        school_id = int(row['school_id'])
+
+        result[school_id] = {
+            'account_count': int(row['account_count']),
+            'exam_count': int(row['exam_count'])
+        }
 
     return result
