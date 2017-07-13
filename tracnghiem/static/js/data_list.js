5632f48459a45e223172922855f3dae4808f459d
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-07-13 19:56:59 +0700

    Refactor js/data_list.js
    Refactorings done:
    * Code styling
    * Declare variables using 'var'
    * Add semicolons after function declarations

--- tracnghiem/static/js/data_list.js
+++ tracnghiem/static/js/data_list.js
@@ -9,12 +9,12 @@ function DataList(url, name, container, render_data_func, prepend){
     this.ajax_add = "/admin/ajax/" + this.url + "/add";
     this.ajax_remove = "/admin/ajax/" + this.url + "/remove";
 
-    this.wrapper = Handlebars.compile("<div class='"+ this.name + "-entry-wrapper' data-id='{{{id}}}'> {{{html}}} </div>");
+    this.wrapper = Handlebars.compile("<div class='" + this.name + "-entry-wrapper' data-id='{{{id}}}'> {{{html}}} </div>");
 
-    this.render = function(id, value){
+    this.render = function (id, value){
         value["_id"] = id;
-        content_html = this.html_content_render(value);
-        complete_html = this.wrapper({
+        var content_html = this.html_content_render(value);
+        var complete_html = this.wrapper({
             id: id,
             html: content_html
         });
@@ -23,46 +23,46 @@ function DataList(url, name, container, render_data_func, prepend){
         } else {
             this.container.prepend(complete_html);
         }
-        element = $("."+this.name+"-entry-wrapper[data-id=\""+id+"\"]");
-        edit_button = element.find("."+this.name+"-button-edit");
+        var element = $("." + this.name + "-entry-wrapper[data-id=\"" + id + "\"]");
+        var edit_button = element.find("." + this.name + "-button-edit");
 
-        remove_button = element.find("."+this.name+"-button-remove");
+        var remove_button = element.find("." + this.name + "-button-remove");
         edit_button.click(this.edit);
         remove_button.click(this.remove);
-    }
+    };
 
-    this.load = function(){
-        $.getJSON(this.ajax_get, $.proxy(function(data){
-            for (i = 0; i < data.entries.length; i++){
-                id = data.entries[i].id;
-                value = data.entries[i].value;
+    this.load = function (){
+        $.getJSON(this.ajax_get, $.proxy(function (data){
+            for (var i = 0; i < data.entries.length; i++) {
+                var id = data.entries[i].id;
+                var value = data.entries[i].value;
 
                 this.render(id, value);
             }
         }, this))
-    }
+    };
 
-    this.add = $.proxy(function(value, callback){
-        $.post(this.ajax_add, {data: JSON.stringify(value)}, $.proxy(function(response){
+    this.add = $.proxy(function (value, callback){
+        $.post(this.ajax_add, {data: JSON.stringify(value)}, $.proxy(function (response){
             this.render(response.id, value);
             callback();
         }, this))
     }, this);
 
-    this.remove = function(id){
-        element = $(".entry-wrapper[data-id=\"" + id + "\"]");
+    this.remove = function (id){
+        var element = $(".entry-wrapper[data-id=\"" + id + "\"]");
         element.remove();
-    }
+    };
 
-    this.edit = function(){
+    this.edit = function (){
         return true;
-    }
+    };
 
-    this.remove = $.proxy(function(event){
-        button = $(event.currentTarget);
+    this.remove = $.proxy(function (event){
+        var button = $(event.currentTarget);
 
-        offending_entry = button.closest("."+this.name+"-entry-wrapper");
-        id = offending_entry.data("id");
+        var offending_entry = button.closest("." + this.name + "-entry-wrapper");
+        var id = offending_entry.data("id");
 
         // TODO: i18n
         swal({
@@ -72,12 +72,13 @@ function DataList(url, name, container, render_data_func, prepend){
             showCancelButton: true,
             confirmButtonText: 'Yes'
         }).then(
-            $.proxy(function(){
-                $.post(this.ajax_remove, {id: id}, function(){
+            $.proxy(function (){
+                $.post(this.ajax_remove, {id: id}, function (){
                     offending_entry.remove();
                 })
             }, this),
-            function(){}
+            function (){
+            }
         );
     }, this);
 
