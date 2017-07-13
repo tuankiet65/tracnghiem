237ae511ddbde57b8e978a80c402f3e68412d41c
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-07-13 20:17:33 +0700

    Refactor js/admin/question.js
    Refactorings done:
    * Code styling
    * Declare variables using 'var'
    * Using new when calling DataList constructor

--- tracnghiem/static/js/admin/question.js
+++ tracnghiem/static/js/admin/question.js
@@ -1,22 +1,21 @@
 // Thanks to this answer on StackOverflow http://stackoverflow.com/a/16315366 (this gave me inspiration)
 // also this tutorial http://jaskokoyn.com/2013/08/08/custom-helpers-handlebars-js-tutorial/
-Handlebars.registerHelper('boldIfCorrectAnswer', function (answer, answer_count, correct_answer) {
-    escaped_answer = Handlebars.escapeExpression(answer);
-    if (answer_count == correct_answer){
+Handlebars.registerHelper('boldIfCorrectAnswer', function (answer, answer_count, correct_answer){
+    var escaped_answer = Handlebars.escapeExpression(answer);
+    var result = '<li class="collection-item">' + escaped_answer + '</li>';
+    if (answer_count === correct_answer) {
         result = '<li class="collection-item teal lighten-3"><strong>' + escaped_answer + '</strong></li>';
-    } else {
-        result = '<li class="collection-item">' + escaped_answer + '</li>';
     }
     return new Handlebars.SafeString(result);
 });
 
 html_template = Handlebars.compile(
-    '<div class="card-panel blue-grey lighten-5">'+
+    '<div class="card-panel blue-grey lighten-5">' +
         '<div class="action-buttons right">' +
             '<button class="question-button-edit waves-effect waves-teal btn-flat"><i class="material-icons">edit</i></button>' +
             '<button class="question-button-remove waves-effect waves-teal btn-flat"><i class="material-icons">delete</i></button>' +
         '</div>' +
-        '<div class="black-text">'+
+        '<div class="black-text">' +
             '<span class="question-statement">{{question}}</span>' +
             '<ul class="collection">' +
                 '{{boldIfCorrectAnswer answer_a 1 correct_answer}}' +
@@ -27,24 +26,24 @@ html_template = Handlebars.compile(
         '</div>' +
     '</div>');
 
-container = $("#question-container")
-QuestionList = DataList("questionset/" + qset_id.toString(), "question", container, html_template, true);
+container = $("#question-container");
+QuestionList = new DataList("questionset/" + qset_id.toString(), "question", container, html_template, true);
 
 QuestionList.load();
 
 function add_question(){
     form.disable_button();
 
-    data = form.get_form_data();
+    var data = form.get_form_data();
 
-    if (!data){
+    if (!data) {
         form.enable_button();
         return false;
     }
 
     data.correct_answer = parseInt($("input[type=radio][name=correct_answer]:checked").val());
 
-    QuestionList.add(data, function(){
+    QuestionList.add(data, function (){
         form.enable_button();
         form.clear_form();
     });
