df36c385285e5431aedfb70ed61a11768f6cb64b
Ho Tuan Kiet <tuankiet65@gmail.com>
2017-07-13 20:18:51 +0700

    Refactor js/admin/school.js
    Refactorings done:
    * Code styling
    * Declare variables using 'var'
    * Using new when calling DataList constructor

--- tracnghiem/static/js/admin/school.js
+++ tracnghiem/static/js/admin/school.js
@@ -1,29 +1,29 @@
 html_template = Handlebars.compile(
-    '<div class="card-panel blue-grey lighten-5">'+
+    '<div class="card-panel blue-grey lighten-5">' +
         '<div class="action-buttons right">' +
             '<button class="school-button-edit waves-effect waves-teal btn-flat"><i class="material-icons">edit</i></button>' +
             '<button class="school-button-remove waves-effect waves-teal btn-flat"><i class="material-icons">delete</i></button>' +
         '</div>' +
-        '<div class="black-text">'+
+        '<div class="black-text">' +
             '<h5>{{name}}</h5>' +
         '</div>' +
     '</div>');
 
-SchoolList = DataList("school", "school", $("#school-container"), html_template);
+SchoolList = new DataList("school", "school", $("#school-container"), html_template);
 
 SchoolList.load();
 
 function add_school(){
     form.disable_button();
 
-    data = form.get_form_data();
+    var data = form.get_form_data();
 
-    if (!data){
+    if (!data) {
         form.enable_button();
         return false;
     }
 
-    SchoolList.add(data, function(){
+    SchoolList.add(data, function (){
         form.clear_form();
         form.enable_button();
     });
