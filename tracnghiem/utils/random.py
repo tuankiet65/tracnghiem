9b872b1ed62e8110ef8f28304627cacae58bbf5c
Ho Tuan Kiet <tuankiet65@gmail.com>
2019-04-25 22:30:19 +0700

    Python: switch from random to secrets

--- tracnghiem/utils/random.py
+++ tracnghiem/utils/random.py
@@ -1,13 +1,13 @@
-import random
+import secrets
 import string
 
 
 def generate_random_string(length: int = 50) -> string:
-    rand = random.SystemRandom()
+    rand = secrets.SystemRandom()
     letters = string.ascii_letters + string.digits
     return ''.join(rand.choice(letters) for _ in range(length))
 
 
 def generate_random_int() -> int:
-    rand = random.SystemRandom()
-    return rand.randint(0, (1 << 31))
+    rand = secrets.SystemRandom()
+    return rand.randint(0, (2 ** 31))
