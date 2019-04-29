03a193540f4f765ee59177f591d4fe0a45715ec8
Ho Tuan Kiet <tuankiet65@gmail.com>
2019-04-29 10:51:08 +0700

    Frontend: fold countdown.js into exam.js

--- tracnghiem/static/js/exam.js
+++ tracnghiem/static/js/exam.js
@@ -106,6 +106,30 @@ function CustomSet(size){
     }
 }
 
+function Countdown(end_time, tick_callback){
+    this.end_time = end_time;
+    this.start_time = moment();
+
+    this.remaining = parseInt((this.end_time - this.start_time) / 1000);
+    this.tick_callback = tick_callback;
+
+    this.timer_callback = function(){
+        this.tick_callback(this.remaining);
+        this.remaining--;
+        if (this.remaining < 0){
+            this.stop();
+        }
+    }.bind(this);
+
+    this.stop = function(){
+        clearInterval(this.timer_id);
+    }.bind(this);
+
+    this.timer_id = setInterval(this.timer_callback, 1000);
+
+    return this;
+}
+
 function Exam(exam, questions){
     this.exam = exam;
     this.questions = questions;
